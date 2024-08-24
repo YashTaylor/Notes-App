a703379671f4df9e8d2123e94b81c5b2c5fbd8cc
YashTailor <yashtailor9287@gmail.com>
2024-08-24 11:18:05 +0530

    Notes App

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.notesapp
+
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.database.NoteDatabase
+import com.example.notesapp.repository.NoteRepository
+import com.example.notesapp.viewmodel.NoteViewModel
+import com.example.notesapp.viewmodel.NoteViewModelFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+
+}
